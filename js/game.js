8704a114dbbdeab73148db7e1a001694e93dc038
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-04-25 16:25:12 +0000

    centered screen

--- js/game.js
+++ js/game.js
@@ -1,10 +1,32 @@
 /* global Phaser */
 
+//Copyright (c) 2022 Brayden Blank All rights reserved
+//
+// Created by: Brayden Blank
+// Created on: April 2022
+// This is the Phaser3 game configuration file
+
+/**
+* Start Phaser Game.
+*/
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
