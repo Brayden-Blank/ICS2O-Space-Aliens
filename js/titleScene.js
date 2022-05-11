97efed0f8e671cd5adcafddedfb28451e1df614d
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-05-11 16:46:49 +0000

    phaser 04 done

--- js/titleScene.js
+++ js/titleScene.js
@@ -4,38 +4,38 @@
 //
 // Created by Brayden Blank
 // Created on: April 2022
-//This is a splash scene
+//This is a title scene
 
-/**
-* This class is the Splash Scene.
+/** 
+* This class is the Title Scene
 */
-class SplashScene extends Phaser.Scene{
+class TitleScene extends Phaser.Scene {
   /** 
-  *This method is the construtor.
+  * This method is the construtor
   */
   constructor() {
-    super({key: "splashScene" })
+    super({ key: "titleScene" })
   }
 
   /** 
   * Can be defined on your own Scenes.
   * This method is called by the Scene Manager when the scene starts,
-  * brefore preload() and create().
-  * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start().
+  * before preload() and create().
+  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /** 
+  /**
   * Can be defined on your own Scenes.
   * Use it to load assets.
   */
   preload() {
-    console.log("Splash Scene")
+    console.log("Title Scene")
   }
 
-  /** 
+  /**
   * Can be defined on your own Scenes.
   * Use it to create your game objects.
   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
@@ -44,15 +44,15 @@ class SplashScene extends Phaser.Scene{
     //pass
   }
 
-  /** 
-  * Should be overridden by your own Scenes.
+  /**
+  * Should be overriden by your own Scenes.
   * This method is called once per game step while the scene is running.
-  * @param {number} time - The current time
+  * @param {number} time - The current time.
   * @param {number} delta - The delta time in ms since the last frame.
   */
   update(time, delta) {
-    this.scene.switch("titleScene")
+    //pass
   }
 }
 
-export default SplashScene
+export default TitleScene
